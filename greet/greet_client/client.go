bfb64cf6818cb86807154032d34230e58cc91f3b
erics <ericstumbo@student.purdueglobal.edu>
2020-03-01 09:39:19 -0500

    adding ignore and fixing server

---
+++ greet/greet_client/client.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/grpc-go-course/greet/greetpb"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main(){
+	fmt.Println("Hello I am a client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("created client: %f", c)
+}
